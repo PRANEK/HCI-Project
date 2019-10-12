f7750890bf86989d5693f0972a4e929bce8faa5b
Pranek Gupta <33083312+PRANEK@users.noreply.github.com>
2019-10-12 20:45:36 +0530

    uploading screens

---
+++ app/src/main/java/com/example/hci_bank/BenificiaryDetails.java
@@ -0,0 +1,14 @@
+package com.example.hci_bank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BenificiaryDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_benificiary_details);
+    }
+}
